f503c0a2c0d1dcbf6ffd8bf6ff3c56ca8478eaaa
Alessandro Capialbi <capialbi.alessandro@studenti-ittfedifermi.edu.it>
2023-04-05 23:20:08 +0200

    Centering Logo

--- JS/scripts.js
+++
@@ -1,5 +0,0 @@
-const toggleButton = document.getElementById('toggle-button');
-const naviList = document.getElementById('navi-list')
-toggleButton.addEventListener('click', () => {
-    naviList.classList.toggle('active');
-})
